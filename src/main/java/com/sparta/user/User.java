78251352a319b2542c9dd475b869b95d3630a7c7
minsik0 <qwew1212@naver.com>
2026-09-25 23:40:33 +0900

    feat: 인증
    - JwtFilter
    - userId 타입 변경

--- src/main/java/com/sparta/user/User.java
+++ src/main/java/com/sparta/user/User.java
@@ -17,7 +17,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    private Long id;
 
     @Column(unique = true, nullable = false)
     private String email;
